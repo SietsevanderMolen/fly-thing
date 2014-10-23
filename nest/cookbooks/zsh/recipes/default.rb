04da3d2775f4d71ce82e9d96289ecf8ba98e8f9b
Sietse van der Molen <sietse@vdmolen.eu>
2014-10-23 09:35:59 +0000

    Change recipe order

--- nest/cookbooks/zsh/recipes/default.rb
+++ nest/cookbooks/zsh/recipes/default.rb
@@ -2,13 +2,13 @@ apt_package "zsh" do
   action :install
 end
 
-execute "install-oh-my-zsh" do
-  action :run
-  command "wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh"
-  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})
-  not_if "test -d /home/vagrant/.oh-my-zsh"
-end
-
 user "vagrant" do
   shell "/bin/zsh"
 end
+
+execute "install-oh-my-zsh" do
+  action :run
+  command "wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | /bin/zsh"
+  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})
+end
+
