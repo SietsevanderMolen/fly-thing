ebacd73b3cbe9a53bbac78ed520c0b7a00259b46
Sietse van der Molen <sietse@vdmolen.eu>
2015-02-01 20:11:42 +0100

    update readme and do self test

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # fly-thing
 
 Flying thing stuff
+
+# Build
+You need the gnat packages for your distribution, at least the ones that provide gprbuild and gnat-gcc.
+
+Then just run make.
--- onboard/src/ravn.adb
+++ onboard/src/ravn.adb
@@ -18,38 +18,40 @@ begin
    PWM_Driver.SetPWMFreq (1000.0); --  Max frequency as per datasheet
    PWM_Driver.SetPin (61, 0); --  Initialize with all off
    Compass.Reset;
-   Passed_Selftest := Compass.Self_Test;
+   if Compass.Self_Test then
+      declare
+         bytes : constant I2C.Byte_Array (1 .. 16) := (others => I2C.Byte (0));
+      begin
+         Ada.Text_IO.Put_Line ("Setting 4 I2C outputs to 0 afap, op/s:");
+         loop
+            declare
+               Finish_Time : Time;
+               Start_Time : constant Time := Clock;
+               Compass_Output : Float;
+            begin
 
-   declare
-      bytes : constant I2C.Byte_Array (1 .. 16) := (others => I2C.Byte (0));
-   begin
-      Ada.Text_IO.Put_Line ("Setting 4 I2C outputs to 0 afap, op/s:");
-      loop
-         declare
-            Finish_Time : Time;
-            Start_Time : constant Time := Clock;
-            Compass_Output : Float;
-         begin
+               for j in Integer range 0 .. 1000 loop
+                  I2C.Write_Array_Data (C => PWM_Driver,
+                                        R => I2C.Register (50), --  Pin 11-15
+                                        Values => bytes);
+                  Compass_Output := Compass.Get_Heading;
+               end loop;
 
-            for j in Integer range 0 .. 1000 loop
-               I2C.Write_Array_Data (C => PWM_Driver,
-                                     R => I2C.Register (50), --  Pin 11 .. 15
-                                     Values => bytes);
-               Compass_Output := Compass.Get_Heading;
-            end loop;
-
-            Finish_Time := Clock;
-            Ada.Float_Text_IO.Put (
-               1000.0 / Float (To_Duration (Finish_Time - Start_Time)),
-               Fore => 4, Aft => 2, Exp => 0
-            );
-            Ada.Text_IO.New_Line;
-            Ada.Float_Text_IO.Put (
-               Compass_Output,
-               Fore => 4, Aft => 2, Exp => 0
-            );
-            Ada.Text_IO.New_Line;
-         end;
-      end loop;
-   end;
+               Finish_Time := Clock;
+               Ada.Float_Text_IO.Put (
+                  1000.0 / Float (To_Duration (Finish_Time - Start_Time)),
+                  Fore => 4, Aft => 2, Exp => 0
+               );
+               Ada.Text_IO.New_Line;
+               Ada.Float_Text_IO.Put (
+                  Compass_Output,
+                  Fore => 4, Aft => 2, Exp => 0
+               );
+               Ada.Text_IO.New_Line;
+            end;
+         end loop;
+      end;
+   else
+      Ada.Text_IO.Put_Line ("HMC5883L didn't pass self test");
+   end if;
 end Ravn;
