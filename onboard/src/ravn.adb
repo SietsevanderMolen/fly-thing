501386de8d35a9b62a2f4aa6b3e2e1e6b730cec1
Sietse van der Molen <sietse@vdmolen.eu>
2015-09-20 01:23:37 +0200

    make quaternions generic

--- onboard/src/ravn.adb
+++ onboard/src/ravn.adb
@@ -11,12 +11,9 @@ with HMC5883L;
 with MPU6050;
 with I2C;
 
-with AHRS;
+with AHRS; use AHRS;
 
 procedure Ravn is
-   package Float_Quaternion is new Quaternions (Float);
-   use Float_Quaternion;
-
    I2C_Bus : aliased I2C.Bus (Adapter_Number => 1); --  /dev/i2c-1
    PWM_Driver : PCA9685.Chip (On_Bus => I2C_Bus'Access,
                               Address => 16#40#);  --  default address
@@ -40,7 +37,6 @@ begin
    declare
       Compass_Output : Vector_Math.Float3;
       IMU_Output : MPU6050.MPU6050_Output;
-      Result : Quaternion;
       Algorithm : AHRS.Mahony := AHRS.Make (Sample_Period => 1.0,
                                             Proportional_Gain => 1.0,
                                             Integral_Gain => 1.0);
@@ -51,7 +47,7 @@ begin
          AHRS.Update (M => Algorithm,
                       Gyroscope => IMU_Output.Gyroscope_Output,
                       Accelerometer => IMU_Output.Accelerometer_Output);
-         Ada.Text_IO.Put_Line ("Q: " & Image (Result));
+         Ada.Text_IO.Put_Line ("Q: " & Float_Quaternion.Image (Algorithm.Output));
       end loop;
    end;
 end Ravn;
